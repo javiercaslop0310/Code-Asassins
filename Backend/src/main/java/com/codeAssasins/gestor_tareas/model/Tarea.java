d75180fd11499d6d9a996d259f7d99e4ad4c6342
jamietannerr <rodriguez.j.me@proton.me>
2026-05-20 11:07:28 +0200

    Backend finalizado: Modelos, repositorios y controladores adaptados al script SQL

---
+++ Backend/src/main/java/com/codeAssasins/gestor_tareas/model/Tarea.java
@@ -0,0 +1,38 @@
+package com.codeAssasins.gestor_tareas.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "tareas")
+public class Tarea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String titulo;
+    private String descripcion;
+    private String estado;
+    private String prioridad;
+    
+    @Column(name = "fecha_limite")
+    private LocalDate fechaLimite;
+    
+    @Column(name = "completada_en")
+    private LocalDateTime completadaEn;
+    
+    @Column(name = "creado_en")
+    private LocalDateTime creadoEn;
+    
+    @Column(name = "actualizado_en")
+    private LocalDateTime actualizadoEn;
+
+    @ManyToOne
+    @JoinColumn(name = "proyecto_id")
+    @JsonIgnore
+    private Proyecto proyecto;
+}
